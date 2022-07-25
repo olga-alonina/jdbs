f3a0274d1e297a193bdaf06088f07301e37e6c5b
austinu <austin@cydeo.com>
2022-07-25 18:48:59 -0400

    avg1_1

--- src/test/resources/avengersHour/day1/practice1.sql
+++ src/test/resources/avengersHour/day1/practice1.sql
@@ -55,17 +55,78 @@ select * from EMPLOYEES
 where LAST_NAME like '%a%a%';
 
 -- 14. display all the employees whose first name starts with 'A'
+select * from EMPLOYEES
+where FIRST_NAME like 'A%';
+
 -- 15. display all the employees whose job_ID contains 'IT'
+select * from EMPLOYEES
+where JOB_ID like '%IT%';
+
 -- 16. display all  unique job_id that end with CLERK in employee table
--- 17.display all  employees first name starts with A and have exactly 4 charactersTotal
+select distinct JOB_ID from EMPLOYEES
+where JOB_ID like '%CLERK';
+
+-- 17.display all employees first name starts with A and have exactly 4 charactersTotal
+select * from EMPLOYEES
+where FIRST_NAME like 'A___';
+
+--display all employees whose first name doesnt have 5 characters in total
+select * from EMPLOYEES
+where FIRST_NAME not like '_____' ;
+
 -- 18. display all the employees whose department id in 50, 80, 100
--- 19. display all employees who does not work in any one of these department id 90, 60,  100, 130, 120
+select * from EMPLOYEES
+where DEPARTMENT_ID in (50,80,100);
+
+-- 19. display all employees who does not work in any of these department id 90, 60,  100, 130, 120
+select * from EMPLOYEES
+where DEPARTMENT_ID not in (90, 60,  100, 130, 120);
+
 -- 20. divide employees into groups by using their job id
+select JOB_ID , count(*) from EMPLOYEES
+group by JOB_ID;
+
 -- 1 display the maximum salaries in each groups
+select JOB_ID, max(SALARY) ,count(*) from EMPLOYEES
+group by JOB_ID;
+
 -- 2 display the minium salaries in each groups
+select JOB_ID, min(SALARY) ,count(*) from EMPLOYEES
+group by JOB_ID;
+
 -- 3 display the average salary of each group
+select JOB_ID, avg(SALARY) ,count(*) from EMPLOYEES
+group by JOB_ID;
+
 -- 4 how many employees are there in each group that have minimum salary of 5000 ?
+select JOB_ID, count(*) from EMPLOYEES
+where SALARY >= 5000
+group by JOB_ID;
+
 -- 5 display the total budgests of each groups
+select JOB_ID, sum(SALARY), count(*) from EMPLOYEES
+group by JOB_ID;
+
+
 -- 21. display all job_id and average salary who work as any of these jobs 'IT_PROG','SA_REP', 'FI_ACCOUNT', 'AD_VP'
+select JOB_ID, avg(SALARY) from EMPLOYEES
+group by JOB_ID
+having JOB_ID in ('IT_PROG','SA_REP', 'FI_ACCOUNT', 'AD_VP');
+
+select JOB_ID, avg(SALARY) from EMPLOYEES
+where JOB_ID in('IT_PROG','SA_REP', 'FI_ACCOUNT', 'AD_VP')
+group by JOB_ID;
+
 -- 22.display max salary  for each department
+select DEPARTMENT_ID, max(SALARY) from EMPLOYEES
+group by DEPARTMENT_ID;
+
 -- 23. display total salary for each department except department_id 50, and where total salary >30000
+select sum(SALARY), DEPARTMENT_ID from EMPLOYEES
+group by DEPARTMENT_ID
+having sum(SALARY) > 30000 and DEPARTMENT_ID <> 50;
+
+select sum(SALARY), DEPARTMENT_ID from EMPLOYEES
+where DEPARTMENT_ID <> 50
+group by DEPARTMENT_ID
+having sum(SALARY) > 30000 ;
