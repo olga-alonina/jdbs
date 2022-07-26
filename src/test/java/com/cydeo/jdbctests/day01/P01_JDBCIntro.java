51748f1d77c2482b8292af0c4984808ab3d72132
Mehmet <mhmtsytrk@gmail.com>
2022-07-26 19:27:15 -0400

    jdbc day01

---
+++ src/test/java/com/cydeo/jdbctests/day01/P01_JDBCIntro.java
@@ -0,0 +1,35 @@
+package com.cydeo.jdbctests.day01;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+public class P01_JDBCIntro {
+    String dbUrl = "jdbc:oracle:thin:@54.211.225.58:1521:XE";
+    String dbUsername = "hr";
+    String dbPassword = "hr";
+
+    @Test
+    public void task1() throws SQLException {
+
+
+        // DriverManager class getConneciton is used for to make connection with database
+        Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+
+        // Statemet helps us to execute Query
+        Statement statement = conn.createStatement();
+
+        // ResutSet stores data that we get from after query execution
+        // rs is just a variable/object name
+        ResultSet rs = statement.executeQuery("select * from regions");
+
+
+
+
+        //close conn
+        rs.close();
+        statement.close();
+        conn.close();
+
+    }
+}
