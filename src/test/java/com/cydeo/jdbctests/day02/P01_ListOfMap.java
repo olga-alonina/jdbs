affd2f906b774ca6a1cc5f85aa746e05ad21b7f8
Mehmet <mhmtsytrk@gmail.com>
2022-07-27 19:12:06 -0400

    jdbc day02

---
+++ src/test/java/com/cydeo/jdbctests/day02/P01_ListOfMap.java
@@ -0,0 +1,19 @@
+package com.cydeo.jdbctests.day02;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class P01_ListOfMap {
+
+    String dbUrl = "jdbc:oracle:thin:@54.211.225.58:1521:XE";
+    String dbUsername = "hr";
+    String dbPassword = "hr";
+
+    @Test
+    public void task1() throws SQLException {
+
+
+    }
+
+}
