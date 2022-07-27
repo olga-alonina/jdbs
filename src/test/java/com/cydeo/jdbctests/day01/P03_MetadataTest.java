8af37244b03acc3fa95aa0bee7bf6b498bfec638
Mehmet <mhmtsytrk@gmail.com>
2022-07-26 21:42:03 -0400

    jdbc day01

--- src/test/java/com/cydeo/jdbctests/day01/P03_MetadataTest.java
+++ src/test/java/com/cydeo/jdbctests/day01/P03_MetadataTest.java
@@ -20,10 +20,78 @@ public class P03_MetadataTest {
 
         // ResutSet stores data that we get from after query execution
         // rs is just a variable/object name
-        ResultSet rs = statement.executeQuery("select * from employees" );
+        ResultSet rs = statement.executeQuery("select * from REGIONS" );
+
+
+        // Database Metadata --> Information bout database
+        DatabaseMetaData databaseMetaData = conn.getMetaData();
+
+
+        System.out.println(databaseMetaData.getUserName());
+        System.out.println(databaseMetaData.getDatabaseProductName());
+        System.out.println(databaseMetaData.getDatabaseProductVersion());
+        System.out.println(databaseMetaData.getDriverName());
+        System.out.println(databaseMetaData.getDriverVersion());
+
+        /*
+        We are not gonna use it for testing purpose.Just information about database
+         */
+
+        // Metadata -- Data about Table
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        // How many column we have 
+        int columnCount = rsmd.getColumnCount();
+        System.out.println(columnCount);
+
+        // What is the column name 5th column 
+        // System.out.println(rsmd.getColumnName(5));
+        // it will return calumnName based on provided index
+
+
+        // PRINT ALL COLUMN NAMES DYNAMICLY
+        System.out.println("=== PRINT ALL COLUMN NAMES DYNAMICLY ===");
+        for (int i = 1; i <=columnCount; i++) {
+            System.out.println(rsmd.getColumnName(i));
+        }
+
+
+        /*
+
+        - HOW TO GET DATA DYNAMICLY
+
+           Resultset --> Table Content
+
+                rs.next()          --> with while loop we are getting data from all table.
+                                        It will jump to each row toget data
+                rs.getString(index) --> To get related cell content
+                rs.getString(columname)
+
+           ResultsetMetadata -->Info about table
+
+                rsmd.getColumnCount() --> it will return columnCount
+                rsmd.getColumnName(index) --> it will give provided index ColumnName
+
+
+         */
 
 
 
+
+
+
+        // PRINT ALL TABLE CONTENT DYNAMICLY
+
+        // iterate each row with while loop
+        while(rs.next()){
+
+            //iterate each column dynamicly
+            for (int i = 1; i <=rsmd.getColumnCount(); i++) {
+                System.out.print(rsmd.getColumnName(i)+"-"+rs.getString(i)+" ");
+
+            }
+            System.out.println();
+        }
 
 
 
