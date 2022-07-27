2f854fcafb5063c799fba6520663afa9429a0686
Mehmet <mhmtsytrk@gmail.com>
2022-07-26 20:18:48 -0400

    jdbc day01

--- src/test/java/com/cydeo/jdbctests/day01/P02_FlexibleNavigation.java
+++ src/test/java/com/cydeo/jdbctests/day01/P02_FlexibleNavigation.java
@@ -17,18 +17,28 @@ public class P02_FlexibleNavigation {
         Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
 
         // Statemet helps us to execute Query
-        Statement statement = conn.createStatement();
+        Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 
         // ResutSet stores data that we get from after query execution
         // rs is just a variable/object name
         ResultSet rs = statement.executeQuery("select first_name,last_name from employees" );
 
+        //first row
+        rs.next();
+        System.out.println(rs.getString(1)+" "+rs.getString(2));
 
+        //second row
+        rs.next();
+        System.out.println(rs.getString(1)+" "+rs.getString(2));
 
+        /*
+        ResultSet.TYPE_SCROLL_INSENSITIVE -->
+        ResultSet.CONCUR_READ_ONLY        -->
+         */
 
-
-
-
+        //last row --> Moves the cursor to the last row in this ResultSet object
+        rs.last();
+        System.out.println(rs.getString(1)+" "+rs.getString(2));
 
 
 
