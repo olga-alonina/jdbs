2350ab5022472c4dfdb2a190a0b25c235c512e66
Mehmet <mhmtsytrk@gmail.com>
2022-07-27 20:41:09 -0400

    jdbc day02 DBUtil

---
+++ src/test/java/com/cydeo/jdbctests/day02/P02_DBUtilPractice.java
@@ -0,0 +1,21 @@
+package com.cydeo.jdbctests.day02;
+
+import org.junit.jupiter.api.Test;
+
+public class P02_DBUtilPractice {
+
+    @Test
+    public void task1() {
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
